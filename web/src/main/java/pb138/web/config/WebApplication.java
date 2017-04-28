f702abeaf6dce2dcf4be70bf43f4e3f88d246f6e
Dominik Gmiterko <d.gmiterko@gmail.com>
2017-04-29 00:24:19 +0200

    Empty spring web application

---
+++ web/src/main/java/pb138/web/config/WebApplication.java
@@ -0,0 +1,13 @@
+package pb138.web.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
